34ce7510c5ae8f700bc8dea956d0e61e214f84cf
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-11-16 08:51:02 -0700

    Initial Commit

---
+++ w03-task.js
@@ -0,0 +1,40 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2) {
+    return number1 +number2;
+}
+
+function addNumbers () {
+    let add1 = Number(document.querySelector('#add1').value);
+    let add2 = Number(document.querySelector('#add2').value);
+
+    document.querySelector('#sum').value = add(add1, add2);
+}
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
