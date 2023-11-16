1ef93679755e1670f65f2d5afc87a51b91ddf2ae
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-11-16 08:51:28 -0700

    Initial Commit

--- w03-task.js
+++ w03-task.js
@@ -3,7 +3,7 @@
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
 function add (number1, number2) {
-    return number1 +number2;
+    return number1 + number2;
 }
 
 function addNumbers () {
