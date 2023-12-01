6bd7f961eb3c4896b92e3e38bf9a77c2455ea2f8
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-11-30 17:40:28 -0700

    Finished

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -3,14 +3,26 @@
 /* Declare and initialize global variables */
 
 const templesElement = document.getElementById("temples");
+let templeList = [];
 
-let templeList = {};
 /* async displayTemples Function */
 
 const displayTemples = (temples) => {
-    temples.foreach(temple => {
+    
+    temples.forEach( (temple) => 
+    {
         let article = document.createElement("article");
         let h3 = document.createElement("h3");
+        h3.textContent = temple.templeName;
+
+        const img = document.createElement('img');
+        img.src = temple.imageUrl;
+        img.alt = temple.location;
+
+        article.appendChild(h3);
+        article.appendChild(img);
+
+        templesElement.appendChild(article);
     });
 }
 
@@ -20,15 +32,41 @@ const displayTemples = (temples) => {
 
 const getTemples = async () => {
   const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+  templeList = await response.json();
+  displayTemples(templeList);
 }
 
 /* reset Function */
-
+const reset = () => {
+    templesElement.innerHTML = "";
+}
 
 /* sortBy Function */
 
+const sortBy = (temples) => {
 
+    reset();
+    let filter = document.querySelector('#sortBy').value;
 
-getTemples();
+    switch (filter)
+    {
+        case 'utah' :
+            displayTemples(temples.filter(temple => temple.location.includes('Utah')));
+            break;
+        case 'notutah' :
+            displayTemples(temples.filter(temple => !temple.location.includes('Utah')));
+            break;
+        case 'older' :
+            displayTemples(temples.filter(temple => new Date(temple.dedicated) < new(1950, 0, 1)));
+            break;
+        case 'all' :
+            displayTemples(temples);
+            break;
+    }
+}
+
 
 /* Event Listener */
+document.querySelector('#sortBy').addEventListener("change", () => { sortBy(templeList)});
+
+getTemples();
